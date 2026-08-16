305b2924f60f1cbf207e46af8d77922a9af1e0ef
hashempoorsadegh214-prog <hashempoor.sadegh214@gmail.com>
2026-08-17 00:56:27 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const map = L.map("map").setView([29.6, 52.5], 7);
 
+L.tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+        attribution: "&copy; OpenStreetMap contributors",
+        maxZoom: 18
+    }
+).addTo(map);
